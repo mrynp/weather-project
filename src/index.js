2ed41fa0e420da633ba242e5b681c257c00d0f55
mrynp <110476086+mrynp@users.noreply.github.com>
2024-03-27 15:04:50 +0800

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -30,13 +30,42 @@ let currentDate = new Date();
 
 currentDateValue.innerHTML = updateDate(currentDate);
 
+function updateWeather(response) {
+  let city = response.data.city;
+  let cityDescription = response.data.condition.description;
+  let cityTemp = Math.round(response.data.temperature.current);
+  let cityHumidity = response.data.temperature.humidity;
+  let cityWind = response.data.wind.speed;
+  let iconUrl = response.data.condition.icon_url;
+
+  let cityElement = document.querySelector("#current-city");
+  let descriptionValue = document.querySelector(".description");
+  let tempValue = document.querySelector(".current-temperature-value");
+  let humidityValue = document.querySelector(".humidity-value");
+  let windValue = document.querySelector(".wind-value");
+  let iconValue = document.querySelector(".current-temperature-icon");
+
+  cityElement.innerHTML = city;
+  descriptionValue.innerHTML = cityDescription;
+  tempValue.innerHTML = cityTemp;
+  humidityValue.innerHTML = cityHumidity;
+  windValue.innerHTML = cityWind;
+  iconValue.innerHTML = `<img src="${iconUrl}">`;
+}
+
+function searchCity(city) {
+  let apiKey = "94c379409f43ba9697t5c0of6330a837";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let query = searchInputElement.value;
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = query;
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+searchCity("manila");
